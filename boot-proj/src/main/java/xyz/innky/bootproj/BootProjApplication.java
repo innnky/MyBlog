8ac50351d318de1848e46920c041e9724ed360b2
Administrator <innkly@163.com>
2022-03-09 10:55:04 +0800

    创建springboot后台

---
+++ boot-proj/src/main/java/xyz/innky/bootproj/BootProjApplication.java
@@ -0,0 +1,13 @@
+package xyz.innky.bootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootProjApplication.class, args);
+    }
+
+}
---
+++ boot-proj/src/test/java/xyz/innky/bootproj/BootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.innky.bootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
